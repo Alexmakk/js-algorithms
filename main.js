db04b9929a1dd5f83539a5c4230698aa942ca656
maksimchuk <maksimchuk.a@merlion.com>
2019-09-27 18:46:18 +0300

    bubble sort

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const binarySearch = require('./src/algorithms/search/binarySearch');
 const quickSort = require('./src/algorithms/sort/quickSort');
 const mergeSort = require('./src/algorithms/sort/mergeSort');
+const bubbleSort = require('./src/algorithms/sort/bubbleSort');
 
 
 
 
 // console.log(binarySearch([1,2,3,4,5,6,7,8,23,43,41,42,45,53,56], 6));
 // console.log(quickSort([39,2,3,8,9,6,20,-1,15,40,3,6]))
-console.log(mergeSort([39,2,3,8,9,6,20,-1,15,40,3,6]))
+// console.log(mergeSort([39,2,3,8,9,6,20,-1,15,40,3,6]))
+console.log(bubbleSort([39,2,3]))
 
 
 
---
+++ src/algorithms/sort/bubbleSort.js
@@ -0,0 +1,18 @@
+//BubbleSort
+
+const bubbleSort = (arr) => {
+    let  check = false;
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - i; j++) {
+            console.log(arr[j])
+        }   
+    }
+
+         
+    
+    // return arrSort;
+
+}
+
+module.exports = bubbleSort;
